087e07a7846f8cdeb6b156b194d7f0f914de6580
glamtistop <glamtistop@gmail.com>
2025-11-10 02:08:21 -0800

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -28,5 +28,24 @@ module.exports = defineConfig({
         ],
       },
     },
+    {
+      resolve: "@medusajs/medusa/notification",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/notification-sendgrid",
+            id: "sendgrid",
+            options: {
+              api_key: process.env.SENDGRID_API_KEY,
+              from: process.env.SENDGRID_FROM_EMAIL,
+              order_placed_template: process.env.SENDGRID_ORDER_PLACED_TEMPLATE,
+            },
+          },
+        ],
+      },
+    },
+  ],
+})
+
   ],
 })
